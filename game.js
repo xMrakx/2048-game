835b3ca4f172ad7e8633587ee2d8ac887bc29959
Brady Madden <bradymadden97@gmail.com>
2017-01-30 13:40:31 -0500

    With tile combination

--- game.js
+++ game.js
@@ -40,6 +40,42 @@ function updateColors(){
 	for(var i = 0; i < 4; i++){
 		for(var j = 0; j < 4; j++){
 			switch(game[i][j]){
+				case 2048:
+					$("#"+i+"-"+j).html("2048");
+					$("#"+i+"-"+j).css("background-color", "DeepSkyBlue");
+					break;
+				case 1024:
+					$("#"+i+"-"+j).html("1024");
+					$("#"+i+"-"+j).css("background-color", "Aquamarine");
+					break;
+				case 512:
+					$("#"+i+"-"+j).html("512");
+					$("#"+i+"-"+j).css("background-color", "PaleGreen");
+					break;
+				case 256:
+					$("#"+i+"-"+j).html("256");
+					$("#"+i+"-"+j).css("background-color", "Gold");
+					break;
+				case 128:
+					$("#"+i+"-"+j).html("128");
+					$("#"+i+"-"+j).css("background-color", "LemonChiffon");
+					break;
+				case 64:
+					$("#"+i+"-"+j).html("64");
+					$("#"+i+"-"+j).css("background-color", "Salmon");
+					break;
+				case 32:
+					$("#"+i+"-"+j).html("32");
+					$("#"+i+"-"+j).css("background-color", "OrangeRed");
+					break;
+				case 16:
+					$("#"+i+"-"+j).html("16");
+					$("#"+i+"-"+j).css("background-color", "Tomato");
+					break;
+				case 8:
+					$("#"+i+"-"+j).html("8");
+					$("#"+i+"-"+j).css("background-color", "SandyBrown");
+					break;
 				case 4:
 					$("#"+i+"-"+j).html("4");
 					$("#"+i+"-"+j).css("background-color", "Tan");
@@ -80,6 +116,10 @@ function moveLeft(){
 				while(game[i][j - k] == 0 && (j-k) >= 0){
 					k++;
 				}
+				if(j-k >= 0){
+					if(checkCollision(game[i][j], i, j, i, j-k))
+						didmove = true;
+				}
 				if(k > 1){
 					game[i][j-k+1] = game[i][j];
 					game[i][j] = 0;
@@ -100,6 +140,10 @@ function moveRight(){
 				var k = 1;
 				while(game[i][j + k] == 0 && (j+k) <= 3){
 					k++;
+				}
+				if(j+k < 4){
+					if(checkCollision(game[i][j], i, j, i, j+k))
+						didmove = true;
 				}
 				if(k > 1){
 					game[i][j+k-1] = game[i][j];
@@ -122,6 +166,10 @@ function moveUp(){
 				while((j-k) >= 0 && game[j - k][i] == 0){
 					k++;
 				}
+				if(j-k >= 0){
+					if(checkCollision(game[j][i], j, i, j-k, i))
+						didmove = true;
+				}
 				if(k > 1){
 					game[j-k+1][i] = game[j][i];
 					game[j][i] = 0;
@@ -143,6 +191,10 @@ function moveDown(){
 				while((j+k) <= 3 && game[j+k][i] == 0){
 					k++;
 				}
+				if(j+k < 4){
+					if(checkCollision(game[j][i], j, i, j+k, i))
+						didmove = true;
+				}
 				if(k > 1){
 					game[j+k-1][i] = game[j][i];
 					game[j][i] = 0;
@@ -155,6 +207,16 @@ function moveDown(){
 	addBlock(didmove);
 }
 
+function checkCollision(num, orow, ocol, row, col){
+	if(num == game[row][col]){
+		game[orow][ocol] = 0;
+		game[row][col] = 2 * num;
+		return true;
+	}else{
+		return false;
+	}
+}
+
 function addBlock(didmove){
 	open = [];
 	for(var i = 0; i < 4; i++){
@@ -164,17 +226,13 @@ function addBlock(didmove){
 			}
 		}
 	}
-	console.log(open);
 	if(open.length == 0){
 		console.log("Game over");
 	}else{
 		if(didmove){
 			var rand = Math.floor(Math.random() * open.length);
-			console.log(open[rand]);
 			var r = Math.floor(open[rand]/4);
-			console.log(r);
 			var c = open[rand]%4;
-			console.log(c);
 			var rand1 = Math.floor(Math.random() * 10);
 			if(rand1 == 9){
 				game[r][c] = 4;
